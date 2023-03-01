a94ed2242f5a6112b0dbb73d0d65d5ad903ed63d
Dima Kozyrev <bvckslvsh@gmail.com>
2023-03-01 12:59:59 +0300

    Initial commit. Blank project.

---
+++ src/test/java/me/bvckslvsh/CRUD/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bvckslvsh.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
